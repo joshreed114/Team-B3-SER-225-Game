a7356cb0ba95742426af12f500ac753d6bb646db
jreed <joshreed114@gmail.com>
2021-12-14 11:55:44 -0500

    Bugfix push test (IntelliJ)

--- src/Collectables/Megacoin.java
+++ src/Collectables/Megacoin.java
@@ -6,6 +6,8 @@ import java.io.File;
 import Level.MapEntityStatus;
 import Level.Player;
 
+import Utils.Point;
+
 // Extends Coin.java to create new Megacoin (a Coin that is worth 10 instead of 1)
 // Resources > "CoinGold.png"
 // Meant to be a coin type that is located in extremely hard-to-reach places, which is why its value is higher than Supercoin
@@ -18,6 +20,9 @@ public class Megacoin extends Coin {
         super(file, x, y);
 	}
 
+    public Megacoin(BufferedImage file, Point location) {
+        super(file, (int) location.x, (int) location.y);
+    }
     @Override
     public void initialize() {
         super.initialize();
--- src/Maps/NewBossLevel.java
+++ src/Maps/NewBossLevel.java
@@ -1,5 +1,9 @@
 package Maps;
 
+import Collectables.Coin;
+import Collectables.Megacoin;
+import Collectables.Invincibility;
+import Collectables.SpeedBoost;
 import Enemies.BossMouse;
 import Enemies.BugEnemy;
 import Enemies.DinosaurEnemy;
@@ -8,10 +12,7 @@ import Engine.ImageLoader;
 import EnhancedMapTiles.EndLevelBox;
 import EnhancedMapTiles.HorizontalMovingPlatform;
 import GameObject.Rectangle;
-import Level.Enemy;
-import Level.EnhancedMapTile;
-import Level.Map;
-import Level.TileType;
+import Level.*;
 import Tilesets.CommonTileset;
 import Tilesets.LevelTwoTileset;
 import Tilesets.MasterMapTileset;
@@ -21,16 +22,17 @@ import Utils.Point;
 import java.util.ArrayList;
 
 public class NewBossLevel extends Map {
-    /*
+
     public NewBossLevel() {
         super("boss_Level2.txt", new LevelTwoTileset(), new Point(6, 14));
     }
-    */
+    /*
     //TESTING
     public NewBossLevel() {
         super("boss_Level2.txt", new LevelTwoTileset(), new Point(76, 35));
     }
-
+    */
+    //TODO: Add functionaltiy to the moving platforms
     @Override
     public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
         ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
@@ -82,6 +84,45 @@ public class NewBossLevel extends Map {
         return enhancedMapTiles;
     }
 
+    // Adds Coins and potions to level
+    @Override
+    public ArrayList<Collectable> loadCollectables() {
+        ArrayList<Collectable> collectables = new ArrayList<>();
+
+        collectables.add(new Coin(
+                ImageLoader.load("CoinBronze.png"), getPositionByTileIndex(22, 26)));
+
+        collectables.add(new Coin(
+                ImageLoader.load("CoinBronze.png"), getPositionByTileIndex(63, 40)));
+        /*
+        collectables.add(new Coin(
+                ImageLoader.load("CoinBronze.png", getPositionByTileIndex(34, 28)));
+
+        collectables.add(new Coin(
+                ImageLoader.load("CoinBronze.png", getPositionByTileIndex(38, 28)));
+
+        collectables.add(new Coin(
+                ImageLoader.load("CoinBronze.png", getPositionByTileIndex(43, 28)));
+
+        collectables.add(new Coin(
+                ImageLoader.load("CoinBronze.png", getPositionByTileIndex(47, 28)));
+        */
+        collectables.add(new Megacoin(
+                ImageLoader.load("CoinGold.png"), getPositionByTileIndex(81, 39)));
+
+        collectables.add(new Invincibility(
+                ImageLoader.load("InvincibilityCollectable.png"), getPositionByTileIndex(68, 39)));
+
+        //Test for invincibility potion at start of level one
+        /*
+        collectables.add(new Invincibility(
+                ImageLoader.load("InvincibilityCollectable.png"),
+                390,
+                725
+        ));
+        */
+        return collectables;
+    }
     @Override
     public ArrayList<Enemy> loadEnemies() {
         ArrayList<Enemy> enemies = new ArrayList<>();
